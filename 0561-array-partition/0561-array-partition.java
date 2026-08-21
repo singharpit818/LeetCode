85bd5339735c31a7b83f1d6af14611ff2bc4ade4
singharpit818 <singharpit818@gmail.com>
2026-08-21 14:41:01 +0530

    Time: 18 ms (30.76%), Space: 49.5 MB (74.01%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        int n = nums.length;
+        for(int i = 0 ; i<n ; i+=2){
+        sum+=Math.min(nums[i],nums[i+1]);
+        }
+        return sum;
+    }
+}
